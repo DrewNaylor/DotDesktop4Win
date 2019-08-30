6bc314ffc808a77b25d80b63702a1ec3bd390735
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-08-30 00:21:18 -0400

    Rename files.

---
+++ ViewDotDesktop/ViewDotDesktopMainWindow.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class aaformMainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.menubarMainWindow = New System.Windows.Forms.MenuStrip()
+        Me.tablelayoutpanelMainWindow = New System.Windows.Forms.TableLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'menubarMainWindow
+        '
+        Me.menubarMainWindow.Location = New System.Drawing.Point(0, 0)
+        Me.menubarMainWindow.Name = "menubarMainWindow"
+        Me.menubarMainWindow.Size = New System.Drawing.Size(509, 24)
+        Me.menubarMainWindow.TabIndex = 0
+        Me.menubarMainWindow.Text = "MenuStrip1"
+        '
+        'tablelayoutpanelMainWindow
+        '
+        Me.tablelayoutpanelMainWindow.ColumnCount = 2
+        Me.tablelayoutpanelMainWindow.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.74656!))
+        Me.tablelayoutpanelMainWindow.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.25344!))
+        Me.tablelayoutpanelMainWindow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tablelayoutpanelMainWindow.Location = New System.Drawing.Point(0, 24)
+        Me.tablelayoutpanelMainWindow.Name = "tablelayoutpanelMainWindow"
+        Me.tablelayoutpanelMainWindow.RowCount = 2
+        Me.tablelayoutpanelMainWindow.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.42061!))
+        Me.tablelayoutpanelMainWindow.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.57938!))
+        Me.tablelayoutpanelMainWindow.Size = New System.Drawing.Size(509, 359)
+        Me.tablelayoutpanelMainWindow.TabIndex = 1
+        '
+        'aaformMainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(509, 383)
+        Me.Controls.Add(Me.tablelayoutpanelMainWindow)
+        Me.Controls.Add(Me.menubarMainWindow)
+        Me.MainMenuStrip = Me.menubarMainWindow
+        Me.Name = "aaformMainWindow"
+        Me.Text = "ViewDotDesktop"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents menubarMainWindow As MenuStrip
+    Friend WithEvents tablelayoutpanelMainWindow As TableLayoutPanel
+End Class
