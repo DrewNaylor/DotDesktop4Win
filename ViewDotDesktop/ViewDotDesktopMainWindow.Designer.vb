2ee0374bc164e6ef9aa294de2c7c8138481e84f9
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-08-30 01:18:02 -0400

    Add labels for file interpretation + resize window.

--- ViewDotDesktop/ViewDotDesktopMainWindow.Designer.vb
+++ ViewDotDesktop/ViewDotDesktopMainWindow.Designer.vb
@@ -31,10 +31,12 @@ Partial Class aaformMainWindow
         Me.tabpageRawFile = New System.Windows.Forms.TabPage()
         Me.textboxRawFileOutput = New System.Windows.Forms.TextBox()
         Me.openfiledialogDotDesktopFile = New System.Windows.Forms.OpenFileDialog()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.labelTypeKey = New System.Windows.Forms.Label()
+        Me.labelNameKey = New System.Windows.Forms.Label()
+        Me.labelExecKey = New System.Windows.Forms.Label()
+        Me.labelUrlKey = New System.Windows.Forms.Label()
+        Me.labelHeaderInfoText = New System.Windows.Forms.Label()
+        Me.linklabelDesktopEntrySpec = New System.Windows.Forms.LinkLabel()
         Me.tablelayoutpanelMainWindow.SuspendLayout()
         Me.tabcontrolFileOutput.SuspendLayout()
         Me.tabpageInterpretation.SuspendLayout()
@@ -45,7 +47,7 @@ Partial Class aaformMainWindow
         '
         Me.menubarMainWindow.Location = New System.Drawing.Point(0, 0)
         Me.menubarMainWindow.Name = "menubarMainWindow"
-        Me.menubarMainWindow.Size = New System.Drawing.Size(509, 24)
+        Me.menubarMainWindow.Size = New System.Drawing.Size(382, 24)
         Me.menubarMainWindow.TabIndex = 0
         Me.menubarMainWindow.Text = "MenuStrip1"
         '
@@ -63,7 +65,7 @@ Partial Class aaformMainWindow
         Me.tablelayoutpanelMainWindow.RowCount = 2
         Me.tablelayoutpanelMainWindow.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29.0!))
         Me.tablelayoutpanelMainWindow.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.tablelayoutpanelMainWindow.Size = New System.Drawing.Size(509, 359)
+        Me.tablelayoutpanelMainWindow.Size = New System.Drawing.Size(382, 244)
         Me.tablelayoutpanelMainWindow.TabIndex = 1
         '
         'textboxDotDesktopFilePath
@@ -72,13 +74,13 @@ Partial Class aaformMainWindow
         Me.textboxDotDesktopFilePath.Location = New System.Drawing.Point(3, 3)
         Me.textboxDotDesktopFilePath.Name = "textboxDotDesktopFilePath"
         Me.textboxDotDesktopFilePath.ReadOnly = True
-        Me.textboxDotDesktopFilePath.Size = New System.Drawing.Size(406, 20)
+        Me.textboxDotDesktopFilePath.Size = New System.Drawing.Size(279, 20)
         Me.textboxDotDesktopFilePath.TabIndex = 0
         '
         'buttonBrowse
         '
         Me.buttonBrowse.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.buttonBrowse.Location = New System.Drawing.Point(415, 3)
+        Me.buttonBrowse.Location = New System.Drawing.Point(288, 3)
         Me.buttonBrowse.Name = "buttonBrowse"
         Me.buttonBrowse.Size = New System.Drawing.Size(91, 23)
         Me.buttonBrowse.TabIndex = 1
@@ -94,19 +96,21 @@ Partial Class aaformMainWindow
         Me.tabcontrolFileOutput.Location = New System.Drawing.Point(3, 32)
         Me.tabcontrolFileOutput.Name = "tabcontrolFileOutput"
         Me.tabcontrolFileOutput.SelectedIndex = 0
-        Me.tabcontrolFileOutput.Size = New System.Drawing.Size(503, 324)
+        Me.tabcontrolFileOutput.Size = New System.Drawing.Size(376, 209)
         Me.tabcontrolFileOutput.TabIndex = 2
         '
         'tabpageInterpretation
         '
-        Me.tabpageInterpretation.Controls.Add(Me.Label4)
-        Me.tabpageInterpretation.Controls.Add(Me.Label3)
-        Me.tabpageInterpretation.Controls.Add(Me.Label2)
-        Me.tabpageInterpretation.Controls.Add(Me.Label1)
+        Me.tabpageInterpretation.Controls.Add(Me.linklabelDesktopEntrySpec)
+        Me.tabpageInterpretation.Controls.Add(Me.labelHeaderInfoText)
+        Me.tabpageInterpretation.Controls.Add(Me.labelUrlKey)
+        Me.tabpageInterpretation.Controls.Add(Me.labelExecKey)
+        Me.tabpageInterpretation.Controls.Add(Me.labelNameKey)
+        Me.tabpageInterpretation.Controls.Add(Me.labelTypeKey)
         Me.tabpageInterpretation.Location = New System.Drawing.Point(4, 22)
         Me.tabpageInterpretation.Name = "tabpageInterpretation"
         Me.tabpageInterpretation.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabpageInterpretation.Size = New System.Drawing.Size(495, 298)
+        Me.tabpageInterpretation.Size = New System.Drawing.Size(368, 183)
         Me.tabpageInterpretation.TabIndex = 0
         Me.tabpageInterpretation.Text = "File output: Interpretation"
         Me.tabpageInterpretation.UseVisualStyleBackColor = True
@@ -117,7 +121,7 @@ Partial Class aaformMainWindow
         Me.tabpageRawFile.Location = New System.Drawing.Point(4, 22)
         Me.tabpageRawFile.Name = "tabpageRawFile"
         Me.tabpageRawFile.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabpageRawFile.Size = New System.Drawing.Size(495, 298)
+        Me.tabpageRawFile.Size = New System.Drawing.Size(368, 183)
         Me.tabpageRawFile.TabIndex = 1
         Me.tabpageRawFile.Text = "File output: Raw"
         Me.tabpageRawFile.UseVisualStyleBackColor = True
@@ -130,7 +134,7 @@ Partial Class aaformMainWindow
         Me.textboxRawFileOutput.Name = "textboxRawFileOutput"
         Me.textboxRawFileOutput.ReadOnly = True
         Me.textboxRawFileOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.textboxRawFileOutput.Size = New System.Drawing.Size(489, 292)
+        Me.textboxRawFileOutput.Size = New System.Drawing.Size(362, 177)
         Me.textboxRawFileOutput.TabIndex = 0
         '
         'openfiledialogDotDesktopFile
@@ -138,47 +142,67 @@ Partial Class aaformMainWindow
         Me.openfiledialogDotDesktopFile.Filter = "Desktop files|*.desktop"
         Me.openfiledialogDotDesktopFile.Title = "Browse for .desktop file"
         '
-        'Label1
+        'labelTypeKey
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(7, 7)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(37, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Type: "
+        Me.labelTypeKey.AutoSize = True
+        Me.labelTypeKey.Location = New System.Drawing.Point(6, 43)
+        Me.labelTypeKey.Name = "labelTypeKey"
+        Me.labelTypeKey.Size = New System.Drawing.Size(37, 13)
+        Me.labelTypeKey.TabIndex = 0
+        Me.labelTypeKey.Text = "Type: "
         '
-        'Label2
+        'labelNameKey
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(7, 20)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(41, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Name: "
+        Me.labelNameKey.AutoSize = True
+        Me.labelNameKey.Location = New System.Drawing.Point(6, 56)
+        Me.labelNameKey.Name = "labelNameKey"
+        Me.labelNameKey.Size = New System.Drawing.Size(41, 13)
+        Me.labelNameKey.TabIndex = 1
+        Me.labelNameKey.Text = "Name: "
         '
-        'Label3
+        'labelExecKey
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(7, 33)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(37, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Exec: "
+        Me.labelExecKey.AutoSize = True
+        Me.labelExecKey.Location = New System.Drawing.Point(6, 69)
+        Me.labelExecKey.Name = "labelExecKey"
+        Me.labelExecKey.Size = New System.Drawing.Size(37, 13)
+        Me.labelExecKey.TabIndex = 2
+        Me.labelExecKey.Text = "Exec: "
         '
-        'Label4
+        'labelUrlKey
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(7, 46)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(35, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "URL: "
+        Me.labelUrlKey.AutoSize = True
+        Me.labelUrlKey.Location = New System.Drawing.Point(6, 82)
+        Me.labelUrlKey.Name = "labelUrlKey"
+        Me.labelUrlKey.Size = New System.Drawing.Size(35, 13)
+        Me.labelUrlKey.TabIndex = 3
+        Me.labelUrlKey.Text = "URL: "
+        '
+        'labelHeaderInfoText
+        '
+        Me.labelHeaderInfoText.AutoSize = True
+        Me.labelHeaderInfoText.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelHeaderInfoText.Location = New System.Drawing.Point(9, 7)
+        Me.labelHeaderInfoText.Name = "labelHeaderInfoText"
+        Me.labelHeaderInfoText.Size = New System.Drawing.Size(244, 16)
+        Me.labelHeaderInfoText.TabIndex = 4
+        Me.labelHeaderInfoText.Text = "How DotDesktop4Win interprets this file:"
+        '
+        'linklabelDesktopEntrySpec
+        '
+        Me.linklabelDesktopEntrySpec.AutoSize = True
+        Me.linklabelDesktopEntrySpec.Location = New System.Drawing.Point(9, 164)
+        Me.linklabelDesktopEntrySpec.Name = "linklabelDesktopEntrySpec"
+        Me.linklabelDesktopEntrySpec.Size = New System.Drawing.Size(221, 13)
+        Me.linklabelDesktopEntrySpec.TabIndex = 5
+        Me.linklabelDesktopEntrySpec.TabStop = True
+        Me.linklabelDesktopEntrySpec.Text = "Latest Freedesktop.org Desktop Entry spec..."
         '
         'aaformMainWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(509, 383)
+        Me.ClientSize = New System.Drawing.Size(382, 268)
         Me.Controls.Add(Me.tablelayoutpanelMainWindow)
         Me.Controls.Add(Me.menubarMainWindow)
         Me.MainMenuStrip = Me.menubarMainWindow
@@ -205,8 +229,10 @@ Partial Class aaformMainWindow
     Friend WithEvents tabpageRawFile As TabPage
     Friend WithEvents textboxRawFileOutput As TextBox
     Friend WithEvents openfiledialogDotDesktopFile As OpenFileDialog
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents labelExecKey As Label
+    Friend WithEvents labelNameKey As Label
+    Friend WithEvents labelTypeKey As Label
+    Friend WithEvents labelUrlKey As Label
+    Friend WithEvents labelHeaderInfoText As Label
+    Friend WithEvents linklabelDesktopEntrySpec As LinkLabel
 End Class
