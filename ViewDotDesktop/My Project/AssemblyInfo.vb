1830dcf992e851f47481e161ec7ad4aa2f252640
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-08-29 22:53:15 -0400

    Add files for viewing program and main solution.

---
+++ ViewDotDesktop/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ViewDotDesktop")>
+<Assembly: AssemblyDescription("View .desktop files as parsed by the DotDesktop4Win partial implementation of Freedesktop.org's Desktop Entry spec.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ViewDotDesktop")>
+<Assembly: AssemblyCopyright("Copyright © 2019 Drew Naylor. Licensed under Apache License 2.0.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cb06d446-7240-47f3-b20f-5021f8eab759")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
