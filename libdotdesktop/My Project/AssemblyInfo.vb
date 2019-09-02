3e7fd14edbb98153cafe8d2ffebec75da306d351
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-09-02 19:08:26 -0400

    Update assemblyinfo and move build folder.
    It's in one location now, but the view app needs it moved.

--- libdotdesktop/My Project/AssemblyInfo.vb
+++ libdotdesktop/My Project/AssemblyInfo.vb
@@ -8,17 +8,18 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("libdotdesktop")> 
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyTitle("libdotdesktop")>
+<Assembly: AssemblyDescription("Get info from .desktop files for the DotDesktop4Win
+ partial implementation of Freedesktop.org's Desktop Entry spec.")>
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("libdotdesktop")> 
-<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyProduct("libdotdesktop")>
+<Assembly: AssemblyCopyright("Copyright © 2019 Drew Naylor. Licensed under Apache License 2.0.")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("89f7453c-5ac5-4fcb-9cdb-aa9d5178b9d5")> 
+<Assembly: Guid("89f7453c-5ac5-4fcb-9cdb-aa9d5178b9d5")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
