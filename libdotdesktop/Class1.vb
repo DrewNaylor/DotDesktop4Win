82267d25638bb7b5299bb400a45e875a3e027ca2
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-09-02 18:58:12 -0400

    Add boilerplate.

--- libdotdesktop/Class1.vb
+++ libdotdesktop/Class1.vb
@@ -1,3 +1,26 @@
-﻿Public Class Class1
+﻿' libdotdesktop - Get info from .desktop files for the DotDesktop4Win
+' partial implementation of Freedesktop.org's Desktop Entry spec.
+' Copyright 2019 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+'
+' This file is a part of the DotDesktop4Win project.
+' Neither DotDesktop4Win nor Drew Naylor are associated with Freedesktop.org.
+'
+'
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may not use this file except in compliance with the License.
+'   You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+'   Unless required by applicable law or agreed to in writing, software
+'   distributed under the License is distributed on an "AS IS" BASIS,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'   See the License for the specific language governing permissions and
+'   limitations under the License.
+
+
+
+Public Class Class1
 
 End Class
