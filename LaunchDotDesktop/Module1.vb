6a2f6fd18c998a6375433cd3cd1e240abade8efd
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-09-04 22:34:52 -0400

    Add boilerplate text to launcher code.

--- LaunchDotDesktop/Module1.vb
+++ LaunchDotDesktop/Module1.vb
@@ -1,4 +1,27 @@
-﻿Module Module1
+﻿' LaunchDotDesktop - Launch .desktop files as parsed by the DotDesktop4Win
+' partial implementation of Freedesktop.org's Desktop Entry spec.
+' Copyright 2019 Drew Naylor
+' (Note that the copyright years include the years left out by the hyphen.)
+'
+' This file is a part of the DotDesktop4Win project.
+' Neither DotDesktop4Win nor Drew Naylor are associated with Freedesktop.org.
+'
+'
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may not use this file except in compliance with the License.
+'   You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+'   Unless required by applicable law or agreed to in writing, software
+'   distributed under the License is distributed on an "AS IS" BASIS,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'   See the License for the specific language governing permissions and
+'   limitations under the License.
+
+
+
+Module Module1
 
     Sub Main()
 
