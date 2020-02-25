40824ecb2f36ae99813652997a95ad9bdd062194
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-02-25 17:25:02 -0500

    Name controls on edit file list form.

--- LaunchDotDesktop/filePathEditor.Designer.vb
+++ LaunchDotDesktop/filePathEditor.Designer.vb
@@ -23,13 +23,13 @@ Partial Class filePathEditor
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.tablelayoutpanelOkCancel = New System.Windows.Forms.TableLayoutPanel()
-        Me.OK_Button = New System.Windows.Forms.Button()
-        Me.Cancel_Button = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.buttonOK = New System.Windows.Forms.Button()
+        Me.buttonCancel = New System.Windows.Forms.Button()
+        Me.labelEditFileList = New System.Windows.Forms.Label()
+        Me.textboxEditStyleManually = New System.Windows.Forms.TextBox()
+        Me.radiobuttonWindowsStyle = New System.Windows.Forms.RadioButton()
+        Me.radiobuttonLinuxStyle = New System.Windows.Forms.RadioButton()
+        Me.radiobuttonEditStyleManually = New System.Windows.Forms.RadioButton()
         Me.tablelayoutpanelOkCancel.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -39,8 +39,8 @@ Partial Class filePathEditor
         Me.tablelayoutpanelOkCancel.ColumnCount = 2
         Me.tablelayoutpanelOkCancel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.tablelayoutpanelOkCancel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tablelayoutpanelOkCancel.Controls.Add(Me.OK_Button, 0, 0)
-        Me.tablelayoutpanelOkCancel.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.tablelayoutpanelOkCancel.Controls.Add(Me.buttonOK, 0, 0)
+        Me.tablelayoutpanelOkCancel.Controls.Add(Me.buttonCancel, 1, 0)
         Me.tablelayoutpanelOkCancel.Location = New System.Drawing.Point(415, 151)
         Me.tablelayoutpanelOkCancel.Name = "tablelayoutpanelOkCancel"
         Me.tablelayoutpanelOkCancel.RowCount = 1
@@ -48,88 +48,88 @@ Partial Class filePathEditor
         Me.tablelayoutpanelOkCancel.Size = New System.Drawing.Size(146, 29)
         Me.tablelayoutpanelOkCancel.TabIndex = 0
         '
-        'OK_Button
+        'buttonOK
         '
-        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
-        Me.OK_Button.Name = "OK_Button"
-        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
-        Me.OK_Button.TabIndex = 0
-        Me.OK_Button.Text = "OK"
+        Me.buttonOK.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.buttonOK.Location = New System.Drawing.Point(3, 3)
+        Me.buttonOK.Name = "buttonOK"
+        Me.buttonOK.Size = New System.Drawing.Size(67, 23)
+        Me.buttonOK.TabIndex = 0
+        Me.buttonOK.Text = "OK"
         '
-        'Cancel_Button
+        'buttonCancel
         '
-        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
-        Me.Cancel_Button.Name = "Cancel_Button"
-        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
-        Me.Cancel_Button.TabIndex = 1
-        Me.Cancel_Button.Text = "Cancel"
+        Me.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.buttonCancel.Location = New System.Drawing.Point(76, 3)
+        Me.buttonCancel.Name = "buttonCancel"
+        Me.buttonCancel.Size = New System.Drawing.Size(67, 23)
+        Me.buttonCancel.TabIndex = 1
+        Me.buttonCancel.Text = "Cancel"
         '
-        'Label1
+        'labelEditFileList
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(556, 26)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Once you've made your changes to the file list (if any), please click OK." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "If you" &
+        Me.labelEditFileList.AutoSize = True
+        Me.labelEditFileList.Location = New System.Drawing.Point(13, 13)
+        Me.labelEditFileList.Name = "labelEditFileList"
+        Me.labelEditFileList.Size = New System.Drawing.Size(556, 26)
+        Me.labelEditFileList.TabIndex = 1
+        Me.labelEditFileList.Text = "Once you've made your changes to the file list (if any), please click OK." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "If you" &
     "'re editing the file list for WSL, please use single quotes instead of double qu" &
     "otes if there are spaces in the path."
         '
-        'TextBox1
+        'textboxEditStyleManually
         '
-        Me.TextBox1.Location = New System.Drawing.Point(16, 125)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(548, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.textboxEditStyleManually.Location = New System.Drawing.Point(16, 125)
+        Me.textboxEditStyleManually.Name = "textboxEditStyleManually"
+        Me.textboxEditStyleManually.Size = New System.Drawing.Size(548, 20)
+        Me.textboxEditStyleManually.TabIndex = 2
         '
-        'RadioButton1
+        'radiobuttonWindowsStyle
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(16, 43)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(93, 17)
-        Me.RadioButton1.TabIndex = 3
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Windows-style"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.radiobuttonWindowsStyle.AutoSize = True
+        Me.radiobuttonWindowsStyle.Location = New System.Drawing.Point(16, 43)
+        Me.radiobuttonWindowsStyle.Name = "radiobuttonWindowsStyle"
+        Me.radiobuttonWindowsStyle.Size = New System.Drawing.Size(93, 17)
+        Me.radiobuttonWindowsStyle.TabIndex = 3
+        Me.radiobuttonWindowsStyle.TabStop = True
+        Me.radiobuttonWindowsStyle.Text = "Windows-style"
+        Me.radiobuttonWindowsStyle.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'radiobuttonLinuxStyle
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(16, 67)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(74, 17)
-        Me.RadioButton2.TabIndex = 4
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "Linux-style"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.radiobuttonLinuxStyle.AutoSize = True
+        Me.radiobuttonLinuxStyle.Location = New System.Drawing.Point(16, 67)
+        Me.radiobuttonLinuxStyle.Name = "radiobuttonLinuxStyle"
+        Me.radiobuttonLinuxStyle.Size = New System.Drawing.Size(74, 17)
+        Me.radiobuttonLinuxStyle.TabIndex = 4
+        Me.radiobuttonLinuxStyle.TabStop = True
+        Me.radiobuttonLinuxStyle.Text = "Linux-style"
+        Me.radiobuttonLinuxStyle.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'radiobuttonEditStyleManually
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(16, 91)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(87, 17)
-        Me.RadioButton3.TabIndex = 5
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "Edit manually"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.radiobuttonEditStyleManually.AutoSize = True
+        Me.radiobuttonEditStyleManually.Location = New System.Drawing.Point(16, 91)
+        Me.radiobuttonEditStyleManually.Name = "radiobuttonEditStyleManually"
+        Me.radiobuttonEditStyleManually.Size = New System.Drawing.Size(87, 17)
+        Me.radiobuttonEditStyleManually.TabIndex = 5
+        Me.radiobuttonEditStyleManually.TabStop = True
+        Me.radiobuttonEditStyleManually.Text = "Edit manually"
+        Me.radiobuttonEditStyleManually.UseVisualStyleBackColor = True
         '
         'filePathEditor
         '
-        Me.AcceptButton = Me.OK_Button
+        Me.AcceptButton = Me.buttonOK
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.CancelButton = Me.Cancel_Button
+        Me.CancelButton = Me.buttonCancel
         Me.ClientSize = New System.Drawing.Size(573, 192)
-        Me.Controls.Add(Me.RadioButton3)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.radiobuttonEditStyleManually)
+        Me.Controls.Add(Me.radiobuttonLinuxStyle)
+        Me.Controls.Add(Me.radiobuttonWindowsStyle)
+        Me.Controls.Add(Me.textboxEditStyleManually)
+        Me.Controls.Add(Me.labelEditFileList)
         Me.Controls.Add(Me.tablelayoutpanelOkCancel)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
@@ -144,11 +144,11 @@ Partial Class filePathEditor
 
     End Sub
     Friend WithEvents tablelayoutpanelOkCancel As System.Windows.Forms.TableLayoutPanel
-    Friend WithEvents OK_Button As System.Windows.Forms.Button
-    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
-    Friend WithEvents Label1 As Windows.Forms.Label
-    Friend WithEvents TextBox1 As Windows.Forms.TextBox
-    Friend WithEvents RadioButton1 As Windows.Forms.RadioButton
-    Friend WithEvents RadioButton2 As Windows.Forms.RadioButton
-    Friend WithEvents RadioButton3 As Windows.Forms.RadioButton
+    Friend WithEvents buttonOK As System.Windows.Forms.Button
+    Friend WithEvents buttonCancel As System.Windows.Forms.Button
+    Friend WithEvents labelEditFileList As Windows.Forms.Label
+    Friend WithEvents textboxEditStyleManually As Windows.Forms.TextBox
+    Friend WithEvents radiobuttonWindowsStyle As Windows.Forms.RadioButton
+    Friend WithEvents radiobuttonLinuxStyle As Windows.Forms.RadioButton
+    Friend WithEvents radiobuttonEditStyleManually As Windows.Forms.RadioButton
 End Class
--- LaunchDotDesktop/filePathEditor.vb
+++ LaunchDotDesktop/filePathEditor.vb
@@ -2,12 +2,12 @@
 
 Public Class filePathEditor
 
-    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonOK.Click
         Me.DialogResult = System.Windows.Forms.DialogResult.OK
         Me.Close()
     End Sub
 
-    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonCancel.Click
         Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.Close()
     End Sub
