e1a4bd6aca0a93f90a8d53d6a8101293aec826aa
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-02-25 19:26:01 -0500

    Add comments.

--- LaunchDotDesktop/filePathEditor.vb
+++ LaunchDotDesktop/filePathEditor.vb
@@ -19,12 +19,19 @@ Public Class filePathEditor
     End Sub
 
     Private Sub radiobuttonWindowsStyle_Click(sender As Object, e As EventArgs) Handles radiobuttonWindowsStyle.Click
+        ' Look at each textbox inside the flow layout panel.
         For Each editBox As TextBox In flowlayoutpanelFileList.Controls
+            ' Check to see if the textbox starts with "/mnt".
             If editBox.Text.StartsWith("/mnt") Then
+                ' If it does, then remove "/mnt/" from the text.
                 editBox.Text = editBox.Text.Remove(0, 5)
+                ' Get the drive letter and capitalize it for later use.
                 Dim driveLetter As String = editBox.Text.Substring(0, 1).ToUpperInvariant
+                ' Remove the old drive letter since we're going to capitalize it.
                 editBox.Text = editBox.Text.Remove(0, 1)
+                ' Re-add the drive letter, and add a colon and the rest of the text.
                 editBox.Text = driveLetter & ":" & editBox.Text
+                ' Replace forward slashes with back slashes.
                 editBox.Text = editBox.Text.Replace("/", "\")
             End If
         Next
