b5292c28b0850fb4af470f2d5d2bfe8e0b6bb1cc
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-28 11:40:49 -0400

    Switched to using the .NET Standard version of the...
    library. Can't use .NET Standard 2.1 with .NET Framework 4.8 for some reason, though.

--- ViewDotDesktop/ViewDotDesktopMainWindow.vb
+++ ViewDotDesktop/ViewDotDesktopMainWindow.vb
@@ -19,8 +19,7 @@
 '   See the License for the specific language governing permissions and
 '   limitations under the License.
 
-
-Imports libdotdesktop
+Imports libdotdesktop_standard
 Imports libscrollswitchtabs
 
 Public Class aaformMainWindow
